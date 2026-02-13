c46e1a65ac4ab2af91c7d6f5383f71e660845e52
Leoul T <leoulteferi1996@gmail.com>
2026-02-13 15:03:57 +0300

    changing the parsing code

--- parser.py
+++ parser.py
@@ -1,20 +1,23 @@
 import networkx as nx
 import pickle
 
-G = nx.Graph()  # undirected graph
+input_file = "C:/Users/hp/Downloads/facebook_combined.txt/facebook_combined.txt"
 
-with open("C:/Users/hp/Downloads/com-youtube.ungraph.txt/com-youtube.ungraph.txt", "r") as f:
+# Create an undirected graph
+G = nx.Graph()
+
+# Read edge list from txt
+with open(input_file, "r") as f:
     for line in f:
-        # Skip comments or empty lines
-        if line.startswith("#") or line.strip() == "":
+        if line.strip() == "":
             continue
-        
         u, v = map(int, line.strip().split())
         G.add_edge(u, v)
 
-with open("youtube_ungraph_pro.pkl", "wb") as f:
+# Save as .pkl
+with open("facebook_friends.pkl", "wb") as f:
     pickle.dump(G, f)
 
-print("Saved correctly as NetworkX graph")
+print(f"Saved correctly as NetworkX graph")
 print("Number of nodes:", G.number_of_nodes())
 print("Number of edges:", G.number_of_edges())
